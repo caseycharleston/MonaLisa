96722f6526dbc277caa29ed3fc5f117cc37b320c
caseycharleston <caseycharleston@gmail.com>
2023-10-10 18:06:45 -0500

    Eye looking

--- UnityRobotHead/Assets/Scripts/Controller.cs
+++ UnityRobotHead/Assets/Scripts/Controller.cs
@@ -17,37 +17,39 @@ public class Controller : MonoBehaviour
     public GameObject user;
     public GameObject leftEye;
     public GameObject rightEye;
-    public GameObject camera;
+    public Camera camera;
     public GameObject plane;
 
     Vector3 eyeOrientVect;
     Vector3 cameraOrientVect;
     Vector3 righteye;
     Vector3 lefteye;
-public GameObject[] babyPlanes; // TODO: store all positions of mini planes in here
+public Transform[] babyPlanes; // TODO: store all positions of mini planes in here
 
 private const int NUM_GRIDS = 16;
 
     void Start () 
     {
         // initalize head
-        Instantiate(head, new Vector3(0,0,0), Quaternion.identity);
-        Instantiate(leftEye, new Vector3(7,-0.65f,-3.8f), Quaternion.identity);
-        Instantiate(rightEye, new Vector3(7,-0.65f,3.8f), Quaternion.identity);
-        Instantiate(camera, new Vector3(50,0,0), Quaternion.Euler(new Vector3(0, -90, 0)));
-        Instantiate(user, new Vector3(60,0,0), Quaternion.Euler(new Vector3(0, -90, 0)));
-        Instantiate(plane, new Vector3(100,0,0), Quaternion.Euler(new Vector3(0, 0, 90)));
+        head = Instantiate(head, new Vector3(0,0,0), Quaternion.identity);
+        leftEye = Instantiate(leftEye, new Vector3(7,-0.65f,-3.8f), Quaternion.identity);
+        rightEye = Instantiate(rightEye, new Vector3(7,-0.65f,3.8f), Quaternion.identity);
+        camera = (Camera) Instantiate(camera, new Vector3(50,0,0), Quaternion.Euler(new Vector3(0, -90, 0)));
+        user = Instantiate(user, new Vector3(60,0,0), Quaternion.Euler(new Vector3(0, -90, 0)));
+        plane = Instantiate(plane, new Vector3(100,0,0), Quaternion.Euler(new Vector3(0, 0, 90)));
 
-        //babyPlanes = new GameObject[NUM_GRIDS];
-        // for (int i = 0; i < NUM_GRIDS; i++) {
-        //     string curPlane = "babyPlane" + (i + 1);
-        //     babyPlanes[i] = GameObject.FindGameObjectsWithTag("babyPlane1")[0];
-        // }
+        // for cross looking
+        eyeOrientVect = new Vector3(1, 0, 0);
+        babyPlanes = new Transform[NUM_GRIDS];
+        for (int i = 0; i < NUM_GRIDS; i++) {
+            babyPlanes[i] = plane.transform.GetChild(i);
+        }
     }
 
     void Update()
     {
-
+        Debug.Log(camera);
+        lookAt(babyPlanes[0].position);
     }
 
     private void updateHeadPosition(float x, float y, float z) {
@@ -70,25 +72,25 @@ private const int NUM_GRIDS = 16;
     // position: gotten from Transform.position
     // 
     public void lookAt(Vector3 target) {
-    	GameObject reye = GameObject.FindGameObjectsWithTag("rightEye")[0];
-        GameObject leye = GameObject.FindGameObjectsWithTag("leftEye")[0];
         // get location, rotation
-        righteye = reye.transform.position;
-        lefteye = leye.transform.position;
-        Vector3 lEyeVect = target - lefteye;
-        Vector3 rEyeVect = target - righteye;
+        Vector3 rEyePos = rightEye.transform.position;
+        Vector3 lEyePos = leftEye.transform.position;
+        Vector3 lEyeVect = target - lEyePos;
+        Vector3 rEyeVect = target - rEyePos;
+        Debug.Log("lEyeVect " + lEyeVect);
+        Debug.Log("rEyeVect " + rEyeVect);
         lEyeVect = Vector3.Normalize(lEyeVect);
         rEyeVect = Vector3.Normalize(rEyeVect);
         // draw laser eyes (superman)
-        Debug.DrawRay(righteye, rEyeVect * 1000000, Color.red, 100000f);
-        Debug.DrawRay(lefteye, lEyeVect * 1000000, Color.red, 100000f);
+        Debug.DrawRay(rEyePos, rEyeVect * 10000000, Color.red, 100000f);
+        Debug.DrawRay(lEyePos, lEyeVect * 10000000, Color.red, 100000f);
         Vector3 lcross = Vector3.Cross(eyeOrientVect, lEyeVect);
         Vector3 rcross = Vector3.Cross(eyeOrientVect, rEyeVect);
         float langle = Vector3.Angle(lEyeVect, eyeOrientVect);
         float rangle = Vector3.Angle(rEyeVect, eyeOrientVect);
         lcross = Vector3.Normalize(lcross);
         rcross = Vector3.Normalize(rcross);
-        reye.transform.rotation = Quaternion.AngleAxis(rangle, rcross);
-        leye.transform.rotation = Quaternion.AngleAxis(langle, lcross);
+        rightEye.transform.rotation = Quaternion.AngleAxis(rangle, rcross);
+        leftEye.transform.rotation = Quaternion.AngleAxis(langle, lcross);
     }
 }
